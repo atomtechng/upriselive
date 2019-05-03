25a5da99f0761b29e97875590ec7dfc9daea96ca
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 20:34:51 +0100

    Add gravatar image

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -17,30 +17,7 @@ class Home extends CI_Controller
     {
         $this->load->view('home');
     }
-
-    // public function request()
-    // {
-          
-    //     $name = $this->input->post('name');
-    //     $email = $this->input->post('email');
-    //     $phonenumber = $this->input->post('phonenumber');
-    //     $link = $this->input->post('link');
-
-    //     $data = array("name" => $name,
-    //         "email" => $email,
-    //         "phonenumber" => $phonenumber,
-    //         "link" => $link,
-    //         "date_registered" => date("Y-m-d H:i:s"));
-
-    //     $this->request_model->create($data);
-
-    //     $Subject ="Welcome to Upriselive!!!";
-    //     $alt_body ="You are about to experince a breakthrough in your career";
-    //     $message= "You are about to experince a breakthrough in your career";
-    //     $this->mail($email, "Upriselive", $name, $message, $Subject, $alt_body);
-
-    //     redirect('thanks');
-    // }
+    
     public function thanks()
     {
         $this->load->view('thanks');
