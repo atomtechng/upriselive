f3703c7d2991b9a199a6dd98463aeee90c5c693b
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 18:17:58 +0100

    Validation

--- application/controllers/Request.php
+++ application/controllers/Request.php
@@ -37,25 +37,36 @@ class Request extends CI_Controller
 
     public function delete($id)
     {
+        
         $request_method = $this->input->server('REQUEST_METHOD');
+        $result = array();
         if($request_method == "delete")
         {
-            $result = array();
-            if($this->request_model->delete($id) == TRUE)
-            {
-                $result["status"] ="success";
-                $result["message"] = "Data with id ". $id. " is deleted";
-            }
-            else
-            {
-                $result["status"] ="fail";
-                $result["message"] = "Data with id ". $id. " couldn't be deleted";
-            }
-            $this->output
+            
+            if (isset($id) && !empty($id)) {
+                
+                if ($this->request_model->delete($id) == true) {
+                    $result["status"] ="success";
+                    $result["message"] = "Data with id ". $id. " is deleted";
+                } else {
+                    $result["status"] ="fail";
+                    $result["message"] = "Data with id ". $id. " couldn't be deleted";
+                }
+                $this->output
                 ->set_content_type('application/json')
                 ->set_output(json_encode($result));
+            }
+            else{
+                $result["status"] ="fail";
+                $result["message"] = "No id specified";
+                $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($result));
+            }
         }
         else{
+            $result["status"] ="fail";
+            $result["message"] = "unauthorized Access";
             $this->output
             ->set_content_type('application/json')
             ->set_output(json_encode(array("status" => "unauthorized")));
